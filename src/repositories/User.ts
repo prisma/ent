079c2f817ce4d1501387e844ab1b57bfbed7c9bd
Flavian DESVERNE <flavian.desverne@epitech.eu>
2019-04-25 17:18:34 +0200

    Fix custom repository

--- src/repositories/User.ts
+++ src/repositories/User.ts
@@ -5,10 +5,4 @@ export class UserRepository extends UserBaseRepository {
   findUsersWithPosts() {
     return this.findMany({ select: { posts: true } });
   }
-
-  async findOne(opts: FindOneRepoOptions<"User">) {
-    const user = this.manager.client.user({ id: "" });
-
-    return this.transformToEntity(user);
-  }
 }
